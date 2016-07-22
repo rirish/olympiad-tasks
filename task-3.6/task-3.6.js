89a5f5ce420760fc791af1027d5ce15ffbb7f24a
Rirish <n.lunina@live.ru>
2016-07-22 20:51:22 +0400

    #40  task-3.6

---
+++ task-3.6/task-3.6.js
@@ -0,0 +1,6 @@
+function getFormValue(){
+
+    var fName = document.getElementById('form1')[0].value;
+    var lName = document.getElementById('form1')[1].value;
+    alert('First name: ' + fName + '\r\n' + 'Last name: ' +lName);
+}
